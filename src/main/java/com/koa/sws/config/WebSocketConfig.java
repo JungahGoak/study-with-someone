cc6aa09c1a5f80f592a59f2c2daa82b6846f009f
JungAh <70677614+Jungahgo@users.noreply.github.com>
2025-11-26 22:51:21 +0900

    chore: front url 세팅

--- src/main/java/com/koa/sws/config/WebSocketConfig.java
+++ src/main/java/com/koa/sws/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.koa.sws.config;
 
 import com.koa.sws.handler.SignalingWebSocketHandler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -12,11 +13,14 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 @RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketConfigurer {
 
+    @Value("${front.url}")
+    private String frontUrl;
+
     private final SignalingWebSocketHandler signalingWebSocketHandler;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(signalingWebSocketHandler, "/signal")
-                .setAllowedOrigins("http://localhost:3000"); // TODO
+                .setAllowedOrigins("http://localhost:3000", frontUrl);
     }
 }
