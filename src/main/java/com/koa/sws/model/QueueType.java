ed5216dcd71e0b279dffd446c1fc81169fc9fd6c
JungAh <70677614+Jungahgo@users.noreply.github.com>
2026-01-24 15:59:39 +0900

    refactor: QueueType enum 전략 패턴 도입으로 publisher/subscriber 분기 제거
    - QueueType enum에 함수형 인터페이스로 큐 동작 정의 (pop, add, getSize, getConnectedPeer)
    - getWaitingSubscriber/getWaitingPublisher 중복 로직을 getWaitingPeer로 통합
    - maxRetries 로직 추가로 무한루프 방지

---
+++ src/main/java/com/koa/sws/model/QueueType.java
@@ -0,0 +1,53 @@
+package com.koa.sws.model;
+
+import com.koa.sws.service.RedisQueueService;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public enum QueueType {
+    PUBLISHER(
+            RedisQueueService::popFromPublishQueue,
+            RedisQueueService::addToPublishQueue,
+            RedisQueueService::getPublishQueueSize,
+            PeerSession::getSubscriber
+    ),
+    SUBSCRIBER(
+            RedisQueueService::popFromSubscribeQueue,
+            RedisQueueService::addToSubscribeQueue,
+            RedisQueueService::getSubscribeQueueSize,
+            PeerSession::getPublisher
+    );
+
+    private final Function<RedisQueueService, String> popFunction;
+    private final java.util.function.BiConsumer<RedisQueueService, String> addFunction;
+    private final Function<RedisQueueService, Long> sizeFunction;
+    private final Function<PeerSession, String> peerGetter;
+
+    QueueType(Function<RedisQueueService, String> popFunction,
+              java.util.function.BiConsumer<RedisQueueService, String> addFunction,
+              Function<RedisQueueService, Long> sizeFunction,
+              Function<PeerSession, String> peerGetter) {
+        this.popFunction = popFunction;
+        this.addFunction = addFunction;
+        this.sizeFunction = sizeFunction;
+        this.peerGetter = peerGetter;
+    }
+
+    public String pop(RedisQueueService queueService) {
+        return popFunction.apply(queueService);
+    }
+
+    public void add(RedisQueueService queueService, String peerId) {
+        addFunction.accept(queueService, peerId);
+    }
+
+    public Long getSize(RedisQueueService queueService) {
+        return sizeFunction.apply(queueService);
+    }
+
+    public String getConnectedPeer(PeerSession peerSession) {
+        return peerGetter.apply(peerSession);
+    }
+}
